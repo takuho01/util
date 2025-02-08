7c6528ea60fa147f6d684f306114eb73ee7abb07
takuho01 <krein.to112@gmail.com>
2025-02-08 19:13:38 +0900

    update

---
+++ mindmap02/mm_file/mindmap02.mm
@@ -0,0 +1,154 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="mindmap02&#xa;について" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1738991649318" STYLE="oval" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.198">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" associatedTemplateLocation="template:/template_mindmap02_ver01_0.mm" show_note_icons="true" fit_to_viewport="false" followedTemplateLocation="template:/template_mindmap02_ver01_0.mm" followedMapLastTime="1738991275973"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT" CHILD_NODES_LAYOUT="AUTO" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BORDER_WIDTH="2.5 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#de8123"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811" BORDER_DASH_LIKE_EDGE="false">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+<stylenode TEXT="Directory" BACKGROUND_COLOR="#ffcccc" STYLE="bubble" BORDER_WIDTH_LIKE_EDGE="false">
+<font NAME="PlemolJP HS Text" SIZE="11"/>
+<edge STYLE="horizontal" COLOR="#999999" DASH="SOLID"/>
+</stylenode>
+<stylenode TEXT="Item">
+<font NAME="PlemolJP HS Text" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#999999" DASH="SOLID"/>
+</stylenode>
+<stylenode TEXT="Note" COLOR="#2787c8" BACKGROUND_COLOR="#f2f2f2" STYLE="bubble" MAX_WIDTH="5 cm" MIN_WIDTH="3 cm">
+<font NAME="PlemolJP HS Text" SIZE="6"/>
+<edge STYLE="horizontal" COLOR="#999999" DASH="SOLID"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="12" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="概要" STYLE_REF="Directory" POSITION="bottom_or_right" ID="ID_664550287" CREATED="1738938927317" MODIFIED="1738991662204">
+<edge COLOR="#7c007c"/>
+<node TEXT="目的" STYLE_REF="Item" ID="ID_1988405746" CREATED="1738938936427" MODIFIED="1738991673269" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<node TEXT="mindmapでメモをとる環境を作る。" STYLE_REF="Note" ID="ID_964082598" CREATED="1738938940721" MODIFIED="1738991732064"/>
+</node>
+<node TEXT="背景" STYLE_REF="Item" ID="ID_368821430" CREATED="1738991694390" MODIFIED="1738991695758" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<node TEXT="textのメモは見にくい。" STYLE_REF="Note" ID="ID_287274077" CREATED="1738991697654" MODIFIED="1738991726590" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt"/>
+</node>
+<node TEXT="設計思想" STYLE_REF="Item" ID="ID_1338497195" CREATED="1738991767104" MODIFIED="1738991769417" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<node TEXT="視認性を重視する" STYLE_REF="Note" ID="ID_1207874314" CREATED="1738991775215" MODIFIED="1738991855574" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt"/>
+<node TEXT="機能はシンプルにする&#xa;（記憶負荷はかけない）" STYLE_REF="Note" ID="ID_1171818126" CREATED="1738991800682" MODIFIED="1738991831102" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt"/>
+</node>
+</node>
+<node TEXT="バージョン" STYLE_REF="Directory" POSITION="bottom_or_right" ID="ID_1605499478" CREATED="1738991741479" MODIFIED="1738991781438" VSHIFT_QUANTITY="17.5 pt">
+<edge COLOR="#7c007c"/>
+<node TEXT="ver01_0" STYLE_REF="Item" ID="ID_307436149" CREATED="1738991877828" MODIFIED="1738991914829">
+<node TEXT="note" STYLE_REF="Item" ID="ID_1840597088" CREATED="1738993513631" MODIFIED="1738993516639">
+<node TEXT="初版。&#xa;以前触った時の知見と、今回のコンセプトをもとに、第一設定を作った。" STYLE_REF="Note" ID="ID_1443703099" CREATED="1738993516858" MODIFIED="1738993561285"/>
+</node>
+<node TEXT="template" STYLE_REF="Item" ID="ID_459433180" CREATED="1738991899678" MODIFIED="1738991927959">
+<node TEXT="template_mindmap02_ver01_0.mm" STYLE_REF="Note" ID="ID_8268179" CREATED="1738992347719" MODIFIED="1738992361891"/>
+</node>
+<node TEXT="環境設定ファイル" STYLE_REF="Item" ID="ID_785136202" CREATED="1738991922518" MODIFIED="1738991935071">
+<node TEXT="setting_ver01_0.freeplaneoptions" STYLE_REF="Note" ID="ID_1319241485" CREATED="1738992314588" MODIFIED="1738992345845"/>
+</node>
+</node>
+</node>
+<node TEXT="追加機能" STYLE_REF="Directory" POSITION="bottom_or_right" ID="ID_137104724" CREATED="1738991746169" MODIFIED="1738992376780" HGAP_QUANTITY="13.5 pt" VSHIFT_QUANTITY="31.5 pt">
+<edge COLOR="#7c007c"/>
+<node TEXT="視認性案" STYLE_REF="Item" ID="ID_1566435599" CREATED="1738991755161" MODIFIED="1738992398366">
+<node TEXT="ver01_0" STYLE_REF="Item" ID="ID_179608056" CREATED="1738992818748" MODIFIED="1738992824812">
+<node TEXT="ディレクトリとアイテムとノート&#xa;の３種類のデザインに絞る&#xa;➡いろいろなツールを使って行く結果、それで十分だと思った" STYLE_REF="Note" POSITION="bottom_or_right" ID="ID_1522738361" CREATED="1738991759970" MODIFIED="1738992567966"/>
+<node TEXT="フォントをPlemolJPにする" STYLE_REF="Note" POSITION="bottom_or_right" ID="ID_902681367" CREATED="1738992574493" MODIFIED="1738992845566"/>
+</node>
+<node TEXT="next" STYLE_REF="Item" ID="ID_148717902" CREATED="1738992831150" MODIFIED="1738992837231">
+<node TEXT="" STYLE_REF="Note" ID="ID_399672506" CREATED="1738992886429" MODIFIED="1738992887483"/>
+</node>
+</node>
+<node TEXT="機能案" STYLE_REF="Item" ID="ID_1969440761" CREATED="1738992393068" MODIFIED="1738992406431">
+<node TEXT="ver01_0" STYLE_REF="Item" ID="ID_506472526" CREATED="1738992853399" MODIFIED="1738992858584">
+<node TEXT="done gitで設定ファイルとテンプレートのバージョン管理" STYLE_REF="Note" POSITION="bottom_or_right" ID="ID_1097014539" CREATED="1738992433225" MODIFIED="1738992598693"/>
+<node TEXT="done ctrl+enterで編集" STYLE_REF="Note" POSITION="bottom_or_right" ID="ID_1731029468" CREATED="1738992407793" MODIFIED="1738992506435"/>
+<node TEXT="タスクノード、完了ノートの作成" STYLE_REF="Note" POSITION="bottom_or_right" ID="ID_1527415562" CREATED="1738992429059" MODIFIED="1738992480199"/>
+<node TEXT="escをホームに戻すショートカットを削除" STYLE_REF="Note" POSITION="bottom_or_right" ID="ID_341255213" CREATED="1738992914810" MODIFIED="1738992927663"/>
+</node>
+<node TEXT="next" STYLE_REF="Item" ID="ID_487769398" CREATED="1738992858916" MODIFIED="1738992860274">
+<node TEXT="" STYLE_REF="Note" ID="ID_875284573" CREATED="1738992893432" MODIFIED="1738992894449"/>
+</node>
+</node>
+<node TEXT="hotkey" STYLE_REF="Item" ID="ID_1254503031" CREATED="1738993388970" MODIFIED="1738993394031">
+<node TEXT="ver01_0" STYLE_REF="Item" ID="ID_150575482" CREATED="1738993394172" MODIFIED="1738993398839">
+<node TEXT="ctrl 1/2/3にそれぞれ、&#xa;Directory/Item/Noteの属性切り替え機能" STYLE_REF="Note" ID="ID_48889137" CREATED="1738993401159" MODIFIED="1738993477101"/>
+<node TEXT="escでhome nodeに戻る機能を消す" STYLE_REF="Note" ID="ID_1982977839" CREATED="1738993479566" MODIFIED="1738993492675"/>
+</node>
+<node TEXT="next" STYLE_REF="Item" ID="ID_1090337550" CREATED="1738993399189" MODIFIED="1738993400417"/>
+</node>
+<node TEXT="その他案" STYLE_REF="Item" ID="ID_618352468" CREATED="1738992867947" MODIFIED="1738992870631">
+<node TEXT="ver01_0" STYLE_REF="Note" ID="ID_1176255079" CREATED="1738992871165" MODIFIED="1738992896269"/>
+<node TEXT="next" STYLE_REF="Item" ID="ID_524917302" CREATED="1738992877880" MODIFIED="1738992879214"/>
+</node>
+</node>
+<node TEXT="機能メモ" STYLE_REF="Directory" POSITION="bottom_or_right" ID="ID_1856773932" CREATED="1738993108762" MODIFIED="1738993115247">
+<edge COLOR="#7c007c"/>
+<node TEXT="設定系" STYLE_REF="Item" ID="ID_1550550183" CREATED="1738993144771" MODIFIED="1738993159885">
+<node TEXT="ショートカットは設定ファイル保存に入らない&#xa;hotkeyで保存する。&#xa;保存場所がここ（強制）&#xa;C:\Users\xxx\AppData\Roaming\Freeplane\1.11.x\accelerators" STYLE_REF="Note" POSITION="bottom_or_right" ID="ID_736290125" CREATED="1738993115876" MODIFIED="1738993284129"/>
+</node>
+</node>
+</node>
+</map>
---
+++ mindmap02/template/template_mindmap02_ver01_0.mm
@@ -0,0 +1,93 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="title" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1738990930766" STYLE="oval" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.755">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" associatedTemplateLocation="template:/standard-1.6.mm" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT" CHILD_NODES_LAYOUT="AUTO" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BORDER_WIDTH="2.5 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#de8123"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811" BORDER_DASH_LIKE_EDGE="false">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+<stylenode TEXT="Directory" BACKGROUND_COLOR="#ffcccc" STYLE="bubble" BORDER_WIDTH_LIKE_EDGE="false">
+<font NAME="PlemolJP HS Text" SIZE="11"/>
+<edge STYLE="horizontal" COLOR="#999999" DASH="SOLID"/>
+</stylenode>
+<stylenode TEXT="Item">
+<font NAME="PlemolJP HS Text" SIZE="10"/>
+<edge STYLE="horizontal" COLOR="#999999" DASH="SOLID"/>
+</stylenode>
+<stylenode TEXT="Note" COLOR="#2787c8" BACKGROUND_COLOR="#f2f2f2" STYLE="bubble" MAX_WIDTH="5 cm" MIN_WIDTH="3 cm">
+<font NAME="PlemolJP HS Text" SIZE="6"/>
+<edge STYLE="horizontal" COLOR="#999999" DASH="SOLID"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="ディレクトリ" STYLE_REF="Directory" POSITION="bottom_or_right" ID="ID_664550287" CREATED="1738938927317" MODIFIED="1738990911126">
+<edge COLOR="#7c007c"/>
+<node TEXT="アイテム" STYLE_REF="Item" ID="ID_1988405746" CREATED="1738938936427" MODIFIED="1738990913158" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<node TEXT="メモ" STYLE_REF="Note" ID="ID_964082598" CREATED="1738938940721" MODIFIED="1738990914020"/>
+</node>
+</node>
+</node>
+</map>
